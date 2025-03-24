3ef92bcaae87c6d56222138b790b61776fd3b6d9
Paige H <paigenerd0625@gmail.com>
2025-03-23 20:05:26 -0500

    update ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,23 +16,38 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
+
+
+    def _check_events(self): ## single underscore indicates a helper method
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
 
-            
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self): ## see, look, another helper method!
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -6,15 +6,29 @@ class Ship:
     def __init__(self, ai_game):
         """Initialzie the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Loaad the ship image and get its recet.
+
+        # Load the ship image and get its recet.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+    
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
