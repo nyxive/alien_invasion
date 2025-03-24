5a6965a1c57103aed8cf46c4fc43bb0eb831f1ee
Paige H <paigenerd0625@gmail.com>
2025-03-24 15:35:48 -0500

    new settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -23,23 +26,31 @@ class AlienInvasion:
             self._update_screen()
             self.ship.update()
 
-
     def _check_events(self): ## single underscore indicates a helper method
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    if event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    if event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                if event.type == pygame.KEYDOWN:
+                   self._check_keydown_events(event)
+                if event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self): ## see, look, another helper method!
         """Update images on the screen, and flip to the new screen."""
--- ship.py
+++ ship.py
@@ -27,9 +27,9 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         # Update the ship's x value, not the rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right: ## this stops it from going over the screen
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0: ## same here
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x.
